0b9d3906f80eedf1e142c26a988f1ce1efc5ba6b
Denis <denis@papyrus.global>
2019-11-12 21:36:50 +0100

    Mainnet

--- papyrus/start-services.sh
+++ papyrus/start-services.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 ROOT_DIR="$( dirname "${SCRIPT_DIR}" )"
 
 # Copy Papyrus bridge settings
-cp ${SCRIPT_DIR}/papyrus-bridge.env ${ROOT_DIR}/.env
+cat ${SCRIPT_DIR}/papyrus-bridge.env ${SCRIPT_DIR}/private.env >${ROOT_DIR}/.env
 
 # Assuming environment variables VALIDATOR_ADDRESS and VALIDATOR_ADDRESS_PRIVATE_KEY specified
 cd ${ROOT_DIR} && docker-compose up -d --build
